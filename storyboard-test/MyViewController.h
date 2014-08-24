c5a58c42a8128e3689b95c29d15b1cfea3b5761f
João Medrado <jcarloz@gmail.com>
2014-08-25 00:16:48 +1000

    Created my own ViewController

---
+++ storyboard-test/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  storyboard-test
+//
+//  Created by João Medrado on 25/08/2014.
+//  Copyright (c) 2014 Joao Medrado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
