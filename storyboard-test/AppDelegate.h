8466cbf8f8afcd2e93d3b64f97ee15430cc019d3
João Medrado <jcarloz@gmail.com>
2014-08-25 00:13:21 +1000

    Initial Commit

---
+++ storyboard-test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  storyboard-test
+//
+//  Created by João Medrado on 25/08/2014.
+//  Copyright (c) 2014 Joao Medrado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
